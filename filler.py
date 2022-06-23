70f5dc6655b3b0812b16371b96f612a716881b02
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2022-06-23 22:56:28 +0800

    заполнение компетенций

--- filler.py
+++ filler.py
@@ -1,3 +1,4 @@
+from msilib.schema import tables
 from tkinter.messagebox import NO
 from docx.enum.text import WD_COLOR_INDEX as colors
 from turtle import width
@@ -47,6 +48,28 @@ class filler:
 								buf = ""
 								first_run = None
 
+	def fill_competitions(self, competitions):
+		saved_competitions = 0
+		table = self.doc.tables[4]
+
+		for j in range(len(competitions)):
+			self.make_new_competition_section(table)
+			n = 0
+			for i in range(len(table.rows) - 3, len(table.rows)):
+				table.rows[i].cells[0].text = str(j)
+				table.rows[i].cells[1].text = competitions[j]["index"]
+				table.rows[i].cells[2].text = competitions[j]["content"]
+				table.rows[i].cells[3].text = competitions[j]["indicators"][n][0]
+				table.rows[i].cells[4].text = competitions[j]["indicators"][n][1]
+				n += 1
+
+	def make_new_competition_section(self, table):
+		for i in range(3):
+			table.add_row()
+			if i > 0:
+				table.rows[-1].cells[0].merge(table.rows[-2].cells[0])
+				table.rows[-1].cells[1].merge(table.rows[-2].cells[1])
+				table.rows[-1].cells[2].merge(table.rows[-2].cells[2])
+
 	def save(self, filename):
 		self.doc.save(filename)
-
